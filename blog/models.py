088a9560e799d854c5af9e627d4cd2c1b7aca309
gulzarka <80506357+gulzarka@users.noreply.github.com>
2023-11-16 05:49:26 +0600

    move queries into QuerySet methods

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,19 @@ class PostQuerySet(models.QuerySet):
             order_by('published_at')
         return posts_at_year
 
+    def popular(self):
+        return self.annotate(likes_count=Count('likes')).\
+            order_by('-likes_count')
+
+    def fetch_with_comments_count(self):
+        posts_with_comments = Post.objects.filter(id__in=self).\
+            annotate(comments_count=Count('comments'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+        for post in self:
+            post.comments_count = count_for_id[post.id]
+        return self
+
 
 class TagQuerySet(models.QuerySet):
     def popular(self):
